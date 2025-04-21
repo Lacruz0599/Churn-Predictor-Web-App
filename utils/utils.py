f549e1ef50dbcc2ec7323ae74f7915aaae688ed5
Cesar <cesareduardocruzcabrera@gmail.com>
2025-04-21 17:18:03 -0600

    documentación utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,20 +1,41 @@
-
 import datetime
 
 import pandas as pd
 # import streamlit as st
 
 
-def add_client_df( client, df, clients_list ):
+def add_client_df(client, df, clients_list):
+    """
+    Adds a new client to the DataFrame and the clients list.
 
+    Args:
+        client (object): The client object containing client details.
+        df (pd.DataFrame): The DataFrame where the client data will be added.
+        clients_list (list): The list of client objects to which the client will be appended.
+
+    Returns:
+        None
+    """
     df.loc[len(df)] = client.to_dict()
-    # df.loc[len(df)] = st.session_state.current_client.to_dict()
     df.reset_index(drop=True, inplace=True)
     clients_list.append(client)
 
 
-def update_current_client(user, date = None, is_month_to_month = None, internet = None, is_optical_fiber = None, is_electronic_check = None):
+def update_current_client(user, date=None, is_month_to_month=None, internet=None, is_optical_fiber=None, is_electronic_check=None):
+    """
+    Updates the attributes of the current client object.
 
+    Args:
+        user (object): The client object to be updated.
+        date (datetime.date, optional): The date to update the client's subscription.
+        is_month_to_month (int, optional): Whether the client is on a month-to-month plan.
+        internet (int, optional): Whether the client has internet service.
+        is_optical_fiber (int, optional): Whether the client has optical fiber internet.
+        is_electronic_check (int, optional): Whether the client pays with an electronic check.
+
+    Returns:
+        None
+    """
     if date:
         user.days = calculate_days(date)
         user.date = date
@@ -29,21 +50,55 @@ def update_current_client(user, date = None, is_month_to_month = None, internet 
 
 
 def delete_last_client(df, list_clients):
+    """
+    Deletes the last client from the DataFrame and the clients list.
 
-    if len(df) > 0:
-        df.drop(df.index[-1], inplace=True)
-        df.reset_index(drop=True, inplace=True)
-        list_clients.pop(-1)
+    Args:
+        df (pd.DataFrame): The DataFrame from which the last row will be removed.
+        list_clients (list): The list of client objects from which the last client will be removed.
+
+    Returns:
+        None
+    """
+    if len(df) == 0:
+        return
+    
+    df.drop(df.index[-1], inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    list_clients.pop(-1)
 
 
-def calculate_days( date ):
+def calculate_days(date):
+    """
+    Calculates the number of days between the given date and today.
 
+    Args:
+        date (datetime.date): The date to calculate the difference from.
+
+    Returns:
+        int: The number of days between the given date and today.
+    """
     today = datetime.date.today()
     return (today - date).days
 
 
-def df_predictions( list_clients ):
+def df_predictions(list_clients):
+    """
+    Creates a DataFrame containing predictions and client details.
 
+    Args:
+        list_clients (list): A list of client objects containing prediction and client details.
+
+    Returns:
+        pd.DataFrame: A DataFrame with the following columns:
+            - "Fecha": The date of the client's subscription.
+            - "Facturación mensual": Whether the client is on a month-to-month plan.
+            - "internet": Whether the client has internet service.
+            - "Tiene fibra óptica": Whether the client has optical fiber internet.
+            - "Paga con cheque electronico": Whether the client pays with an electronic check.
+            - "Probabilidad": The predicted probability of churn.
+            - "churn": Whether the client is predicted to churn.
+    """
     dates = []
     is_month_to_month = []
     internet = []
